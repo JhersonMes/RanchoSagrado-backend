6f20eb2c86804df82d72d45501a9166de1eee920
JhersonMes <mishifug@gmail.com>
2026-04-15 08:16:39 +0200

    Se agrego atributos a la entidad creada Client

--- src/main/java/com/rancho/model/Client.java
+++ src/main/java/com/rancho/model/Client.java
@@ -1,4 +1,31 @@
 package com.rancho.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+//@Table(name="categories", schema="sales")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include // Identidad única
+    private Integer idClient;
+    @Column(nullable = false, length = 8)//")
+    private Integer dni;
+    @Column(nullable = false, length = 70)//name="nombre")
+    private String name;
+    @Column(nullable = false, length = 30)//name="nombre")
+    private String email;
+    @Column(nullable = false, length = 10)//name="nombre")
+    private Integer numb;
 }
