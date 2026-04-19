dd7cc1436ae5e1fb9d4ac9efc54a4e72f33ec674
ANTHOX123 <huaripatabrayan61@gmail.com>
2026-04-19 02:33:30 -0500

    Update Payment.java

--- src/main/java/com/rancho/model/Payment.java
+++ src/main/java/com/rancho/model/Payment.java
@@ -19,13 +19,13 @@ public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
-    private Integer idPayment;
+    private Integer idPayment; //MODIFICADOR DE ACCESO
 
     @Column(nullable = false)
     private LocalDateTime paymentDate;
 
     @Column(nullable = false, precision = 10, scale = 2)
-    private BigDecimal amount;
+    private BigDecimal amount; //BigDecimal----> MANEJAR NUMEROS DECIMALES CON ALTA PRECISIÓN
 
     @Column(nullable = false, length = 30)
     private String paymentMethod; // EFECTIVO, TARJETA, YAPE, PLIN
