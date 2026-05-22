751541827c3c4123dba9e44940a8b866c5f79a44
JhersonMes <mishifug@gmail.com>
2026-05-22 14:56:57 +0200

    Add: Se subio el generic

--- src/main/java/com/rancho/repository/IGenericRepository.java
+++ src/main/java/com/rancho/repository/IGenericRepository.java
@@ -3,7 +3,7 @@ package com.rancho.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
-@NoRepositoryBean // no se genera el bin de la clase generica
+@NoRepositoryBean // no se genera el bin de la clase generica dad
 
 public interface s<T, ID> extends JpaRepository<T, ID> {
 
