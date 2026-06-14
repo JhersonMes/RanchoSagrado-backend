fb85ee6a8da74a7d1425c6b40f14022a84218874
DiegoFabi <DFabianSevillano@Gmail.Com>
2026-06-13 23:32:33 -0500

    feat: agregar findOneByUsername y changePassword en IUserRepository

--- src/main/java/com/rancho/repository/IUserRepository.java
+++ src/main/java/com/rancho/repository/IUserRepository.java
@@ -1,6 +1,16 @@
 package com.rancho.repository;
 
 import com.rancho.model.User;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface IUserRepository extends IGenericRepository<User, Integer> {
+    User findOneByUsername(String username);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE User u SET u.password = :password WHERE u.username = :username") //JQPL
+    void changePassword(@Param("username") String username, @Param("password") String newPassword);
 }
