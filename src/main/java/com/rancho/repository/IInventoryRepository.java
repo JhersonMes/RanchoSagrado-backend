eb0c3e5443c58921c1ce311db7a0a1702bb54f93
ANTHOX123 <huaripatabrayan61@gmail.com>
2026-06-18 23:31:13 -0500

    "Documenta interfaz IInventoryRepository"

--- src/main/java/com/rancho/repository/IInventoryRepository.java
+++ src/main/java/com/rancho/repository/IInventoryRepository.java
@@ -2,5 +2,6 @@ package com.rancho.repository;
 
 import com.rancho.model.Inventory;
 
+// Repositorio para gestionar las operaciones CRUD de la entidad Inventory
 public interface IInventoryRepository extends IGenericRepository<Inventory, Integer> {
 }
